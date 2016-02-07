adcd8c36c796adb4e26ab5bc4f9ad8ceaf72aac3
Keloran <keloran@nordicarts.net>
2016-02-07 14:15:14 +0000

    Initial Commit

---
+++ NAToolKit/NAToolKit.h
@@ -0,0 +1,19 @@
+//
+//  NAToolKit.h
+//  NAToolKit
+//
+//  Created by Max Hooton on 07/02/2016.
+//  Copyright © 2016 NordicArts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NAToolKit.
+FOUNDATION_EXPORT double NAToolKitVersionNumber;
+
+//! Project version string for NAToolKit.
+FOUNDATION_EXPORT const unsigned char NAToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NAToolKit/PublicHeader.h>
+
+
